d0cb573d1f28e87a2a07ef15bb03999245a12d60
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-06 23:12:49 -0500

    Creating multiple routes

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  response.send("<h1>Hello World</h1>");
+  res.send("<h1>Hello World</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("<h1>Contact me at: brandon@gmail.com</h1>");
+});
+
+app.get("/about", function (req, res) {
+  res.send("Hi, my name is Brandon Hernandez and I like to code!");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("I like F1 and Golf");
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
